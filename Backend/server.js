45f9abf49746640b2b91ba3af2ab1440d170df57
Rishu Raj <rrdebug7@gmail.com>
2026-06-25 16:59:09 +0530

    added Serivce folder

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 const app = require("./src/app");
 const connectDB = require("../Backend/src/config/database");
+const invokeGeminiAi= require("./src/services/ai.service")
 const PORT = process.env.PORT || 3000;
 connectDB();
 app.listen(PORT, () => {
+  invokeGeminiAi();
   console.log(`Server is running on ${PORT}`);
 });
